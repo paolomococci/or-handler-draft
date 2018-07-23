9e976c0e8a4dd27da3335161d52c085c0b455f7e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-07-23 17:39:20 +0200

    Create Supplier.java

---
+++ src/main/java/local/example/draft/model/entity/Supplier.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ * Copyright 2018 paolo mococci
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package local.example.draft.model.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author paolo mococci
+ */
+
+@Entity
+public class Supplier 
+        implements Serializable {
+    
+    private static final long serialVersionUID = -1359137517678696191L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    private String code;
+    private String name;
+
+    public Supplier() {
+        super();
+    }
+
+    public Supplier(String code, String name) {
+        super();
+        this.code = code;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" + "id=" + id + ", code=" + code + 
+                ", name=" + name + '}';
+    }
+}
