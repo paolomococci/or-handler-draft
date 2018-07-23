74141c7e2045d48189a55c6a2ed99d75c55f772f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-07-23 17:37:14 +0200

    Create Employee.java

---
+++ src/main/java/local/example/draft/model/entity/Employee.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ * Copyright 2018 paolo mococci
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package local.example.draft.model.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author paolo mococci
+ */
+
+@Entity
+public class Employee 
+        implements Serializable {
+    
+    private static final long serialVersionUID = 2609923638971663255L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    private String code;
+    private String name;
+    private String surname;
+    private String role;
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(String code, String name, String surname, String role) {
+        this.code = code;
+        this.name = name;
+        this.surname = surname;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", code=" + code + ", name=" + name + 
+                ", surname=" + surname + ", role=" + role + '}';
+    }
+}
