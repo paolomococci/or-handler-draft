46109dd816d42d29612a62e445f65fdf95d03ac1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-07-23 17:40:57 +0200

    Create Warehouse.java

---
+++ src/main/java/local/example/draft/model/entity/Warehouse.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ * Copyright 2018 paolo mococci
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package local.example.draft.model.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author paolo mococci
+ */
+
+@Entity
+public class Warehouse 
+        implements Serializable {
+    
+    private static final long serialVersionUID = -8546236015392430799L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    private String position;
+    private int capacity;
+
+    public Warehouse() {
+        super();
+    }
+
+    public Warehouse(int capacity) {
+        super();
+        this.capacity = capacity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Warehouse{" + "id=" + id + ", position=" + position + 
+                ", capacity=" + capacity + '}';
+    }
+}
