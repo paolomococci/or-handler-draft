9652d94c9628afdfdb8eba1378ce618f5d74e846
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-07-23 15:44:47 +0200

    Create CarrierRepositoryMockMvcTests.java

---
+++ src/test/java/local/example/draft/model/repository/CarrierRepositoryMockMvcTests.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ * Copyright 2018 paolo mococci
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package local.example.draft.model.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+/**
+ *
+ * @author paolo mococci
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CarrierRepositoryMockMvcTests {
+    
+    @Autowired
+    private MockMvc mock;
+    
+    @Autowired
+    private CarrierRepository carrier;
+    
+    @Before
+    public void deleteAll() 
+            throws Exception {
+        carrier.deleteAll();
+    }
+    
+    @Test
+    public void returnItemRepositoryIndexTest() 
+            throws Exception {
+        mock.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._links.carrier").exists());
+    }
+}
